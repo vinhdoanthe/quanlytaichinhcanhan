5a3ff85bd9a27f855720ee414809cd9d09ee7b69
Vinh Doan-The <vinhdt@teky.edu.vn>
2023-09-05 11:40:41 +0700

    Add footer

--- conf/settings.py
+++ conf/settings.py
@@ -142,6 +142,9 @@ USE_THOUSAND_SEPARATOR = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
